571faa4da557a5df62fd60cbc78d3dd2f4f0a44b
MartinOrji <martin.orji.jnr@gmail.com>
2024-10-25 13:32:43 -0500

    added routes

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Length, NumberRange, Email, Optional
 
 
 class UserForm(FlaskForm):
@@ -10,4 +10,55 @@ class UserForm(FlaskForm):
     last_name = StringField('Last Name', validators=[InputRequired])
     password = PasswordField("Password", validators=[InputRequired()])
 
+class LoginForm(FlaskForm):
+    """Login form."""
 
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(min=1, max=20)],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=55)],
+    )
+
+class RegisterForm(FlaskForm):
+    """User registration form."""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(min=1, max=20)],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=55)],
+    )
+    email = StringField(
+        "Email",
+        validators=[InputRequired(), Email(), Length(max=50)],
+    )
+    first_name = StringField(
+        "First Name",
+        validators=[InputRequired(), Length(max=30)],
+    )
+    last_name = StringField(
+        "Last Name",
+        validators=[InputRequired(), Length(max=30)],
+    )
+
+
+class FeedbackForm(FlaskForm):
+    """Add feedback form."""
+
+    title = StringField(
+        "Title",
+        validators=[InputRequired(), Length(max=100)],
+    )
+    content = StringField(
+        "Content",
+        validators=[InputRequired()],
+    )
+
+
+class DeleteForm(FlaskForm):
+    """Delete form -- this form is intentionally blank."""
--- models.py
+++ models.py
@@ -24,6 +24,8 @@ class User(db.Model):
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30),nullable=False)
 
+    feedback = db.relationship("Feedback", backref="user", cascade="all,delete")
+
 
     @classmethod 
     def register(cls, username, pwd,first_name, last_name, email):
@@ -31,12 +33,43 @@ class User(db.Model):
         hashed = bcrypt.generate_password_hash(pwd)
         hashed_utf8 = hashed.decode("utf8")
 
-        return cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name)
+        return cls(username=username, password=hashed_utf8,  first_name=first_name, last_name=last_name, email=email)
 
 
+    @classmethod
+    def authenticate(cls, username, password):
+        """Validate that user exists & password is correct.
+
+        Return user if valid; else return False.
+        """
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
+        else:
+            return False
+
+    
+    
     def __repr__(self):
 
         p = self
         return f'username: {p.username}, password:{p.password}, first_name:{p.first_name}, last_name:{p.last_name} '
 
     
+
+
+class Feedback(db.Model):
+    """Feedback."""
+
+    __tablename__ = "feedback"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(
+        db.String(20),
+        db.ForeignKey('users.username'),
+        nullable=False,
+    )
